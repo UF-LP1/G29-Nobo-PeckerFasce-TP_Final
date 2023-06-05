84709dd7ffa4c2d18ce7eb87567ae01ba096860e
Paula Pecker <paulapeckerfasce@gmail.com>
2023-06-05 18:44:16 -0300

    hospital.h

--- cHOSPITAL.h
+++ cHOSPITAL.h
@@ -1,5 +1,22 @@
+#ifndef _cHOSPITAL_H
+#define _cHOSPITAL_H
+#include cPACIENTE
 #pragma once
 class cHOSPITAL
 {
+private:
+
+public:
+	const string nombre;
+	const string direccion;
+	
+	cHOSPITAL(string nombre, string direccion);
+	~cHOSPITAL();
+
+	list <cPACIENTE> buscar_por_tratamiento_y_tumor(eRadioterapia tratamiento, eTipoTumor tumor);
+	list <cPACIENTE> buscar_por_menos_del_5porciento();
+	void imprimirListado();
+	string to_string();
+	void imprimir();
 };
 
