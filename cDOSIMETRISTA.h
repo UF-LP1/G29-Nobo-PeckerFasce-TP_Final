89ba854dc4ea5a546f40c7a398633ba50674026f
agostina nobo <agosnobo@yahoo.com>
2023-06-05 19:19:43 -0300

    dosimetrista y ficha

--- cDOSIMETRISTA.h
+++ cDOSIMETRISTA.h
@@ -1,7 +1,19 @@
 #pragma once
 #include "cPERSONA.h"
+#include "cFICHA.h"
+
+#ifndef _cDOSIMETRISTA_H
+#define _cDOSIMETRISTA_H
+
 class cDOSIMETRISTA :
     public cPERSONA
 {
+public:
+
+    cDOSIMETRISTA(string nombre, string dni);
+    ~cDOSIMETRISTA();
+
+    void generar_dosis(cFICHA ficha);
 };
 
+#endif
