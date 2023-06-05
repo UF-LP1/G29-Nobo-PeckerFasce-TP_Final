87d07993954fd17239a20873b3225e7d00356af3
agostina nobo <agosnobo@yahoo.com>
2023-06-05 18:43:04 -0300

    persona

--- cPERSONA.h
+++ cPERSONA.h
@@ -1,5 +1,33 @@
 #pragma once
+#ifndef _cPERSONA_H
+#define _cPERSONA_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+using namespace std;
+
 class cPERSONA
 {
-};
+
+public:
+
+    cPERSONA(string nombre, string dni);
+    ~cPERSONA();
+
+    void set_telefono(string telefono);
+    string get_telefono();
+    string get_nombre();
+    string get_dni();
+    virtual string to_string();
+    void imprimir();
+
+protected:
+    const string nombre;
+    const string dni;
+    string telefono;
+
+}
+#endif
+
 
