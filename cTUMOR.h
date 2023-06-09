9d6a02071dcbe407f75fc7354ea860eed8f87652
agostina nobo <agosnobo@yahoo.com>
2023-06-09 09:25:35 -0300

    tumor

--- cTUMOR.h
+++ cTUMOR.h
@@ -11,20 +11,17 @@ public:
 	cTUMOR(eTipoTumor tipo);
 	~cTUMOR();
 	string to_string();
-
+	void imprimir();
+	void set_tamanio(eTamanioTumor tamanio);
+	void set_dosisAcumTumor(unsigned int dosisAcumTumor);
+	eTamanioTumor get_tamanio();
+	unsigned int get_dosisAcumTumor();
 
 	const eTipoTumor tipo;
-
-
 
 private:
 	eTamanioTumor tamanio;
 	unsigned int dosisAcumTumor;
-
-
-
-
-
 
 };
 
