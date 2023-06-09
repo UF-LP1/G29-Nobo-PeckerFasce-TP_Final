8117bb3ff4aff36be06dec4cce0510d2c639cc0f
agostina nobo <agosnobo@yahoo.com>
2023-06-09 13:18:59 -0300

    terapias to_string

--- cSISTEMICA.cpp
+++ cSISTEMICA.cpp
@@ -3,8 +3,19 @@
 cSISTEMICA::cSISTEMICA() :cRADIOTERAPIA(sistemica) {
 
 }
+
 cSISTEMICA::~cSISTEMICA() {
 
+}
+
+unsigned int cSISTEMICA::ajustar_dosis() {
+
+}
+
+string cSISTEMICA::to_string() {
+	stringstream ss;
+	ss << "Terapia sistemica con dosis maxima de " << cSISTEMICA::dosisMax << ". El tratamiento consta de " << this->frecuenciaSemanalPorTumor << " sesiones por semana, con una dosis de " << this->dosisPorSesion << " en cada una.";
+	return ss.str();
 }
 
 
