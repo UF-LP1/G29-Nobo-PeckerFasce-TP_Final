bdbb26c720e719a75bd09978253aef3abab3df72
agostina nobo <agosnobo@yahoo.com>
2023-06-11 20:05:42 -0300

    generar dosis

--- cDOSIMETRISTA.cpp
+++ cDOSIMETRISTA.cpp
@@ -15,18 +15,26 @@ void cDOSIMETRISTA::generar_dosis(cPACIENTE paciente) {
 	for (int i = 0; i < paciente.get_ficha().get_tumores().size();i++)
 	{
 		unsigned int dosis;
-	//dynamic_cast del puntero de radioterapia 
+		//dynamic_cast del puntero de radioterapia para que llame al ajustar dosis correspondiente
 		if (dynamic_cast<cBRAQUITERAPIA*>(paciente.get_ficha().get_tumores()[i].get_tratamiento()) != nullptr)
 		{
-			//dynamic_cast<Cuadrado*>(aux)->imprimir();
 			dosis=dynamic_cast<cBRAQUITERAPIA*>(paciente.get_ficha().get_tumores()[i].get_tratamiento())->ajustar_dosis(paciente.get_ficha().get_tumores()[i].get_tamanio());
 		}
-			
-			
-
+		else if (dynamic_cast<cSISTEMICA*>(paciente.get_ficha().get_tumores()[i].get_tratamiento()) != nullptr)
+		{
+			dosis = dynamic_cast<cSISTEMICA*>(paciente.get_ficha().get_tumores()[i].get_tratamiento())->ajustar_dosis(paciente.get_ficha().get_tumores()[i].get_tamanio());
+		}
+		else if (dynamic_cast<cHAZEXTERNO*>(paciente.get_ficha().get_tumores()[i].get_tratamiento()) != nullptr)
+		{
+			dosis = dynamic_cast<cHAZEXTERNO*>(paciente.get_ficha().get_tumores()[i].get_tratamiento())->ajustar_dosis(paciente.get_ficha().get_tumores()[i].get_tamanio());
+		}
+		
+		//guardo la dosis que me devuelva la funcion en la maxima de ese tumor
+		paciente.get_ficha().get_tumores()[i].set_dosisMaxTumor(dosis);
 
 	}
 
+	return;
 
 }
 
