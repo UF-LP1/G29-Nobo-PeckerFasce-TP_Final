838a445b5567bbc5dbcab68981a06d9dd5521fdf
Paula Pecker <paulapeckerfasce@gmail.com>
2023-06-18 18:42:21 -0300

    pusheo

--- cRADIOTERAPIA.h
+++ cRADIOTERAPIA.h
@@ -23,8 +23,7 @@ public:
 	void set_frecuenciaSemanalPorTumor(unsigned int frecuenciaSemanalPorTumor);
 	unsigned int get_dosisPorSesion();
 	unsigned int get_frecuenciaSemanalPorTumor();
-	virtual unsigned int ajustar_dosis(eTamanioTumor tamanio) = 0; 
-	//genero con random una dosis maxima para cada tumor
+	virtual unsigned int ajustar_dosis(eTamanioTumor tamanio) = 0; //genero con random una dosis maxima para cada tumor
 	virtual string to_string()=0;
 	friend ostream& operator<<(ostream& out, cRADIOTERAPIA& radioterapia);
 
--- excepciones.h
+++ excepciones.h
@@ -8,3 +8,8 @@ public:
 		return "El tratamiento del paciente aun no ha comenzado";
 	}
 };
+class exNoHayTumores :public exception {
+	const char* what()const throw() {
+		return "El paciente no ha sido diagnosticado con ninún tumor";
+	}
+};
